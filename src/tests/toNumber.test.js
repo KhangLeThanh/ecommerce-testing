e11725b8506a39393087a0598e5e1d92bcf1dba3
Khang Le Thanh <khang.lethanh@lionbridge.com>
2021-11-05 11:11:59 +0100

    unit test for toNumber

--- src/tests/toNumber.test.js
+++ src/tests/toNumber.test.js
@@ -1,3 +1,35 @@
 import chai from "chai"
 import toNumber from "../toNumber.js"
+import isObject from '../isObject.js'
+import isSymbol from '../isSymbol.js'
 const expect = chai.expect
+const mySymbol = Symbol('hello');
+
+describe("toNumber", () => {
+
+    before(function() {
+        // runs once before the first test in this block
+      });
+    it("converting a string to a number", () =>{
+        expect(toNumber('3.2')).to.eql(3.2);
+    });  
+    it("returning a same number", () =>{
+        expect(toNumber(3.2)).to.eql(3.2);
+    });  
+    it("returning an Infinity", () =>{
+        expect(toNumber(Infinity)).to.eql(Infinity);
+    }); 
+    it("returning a very small number", () =>{
+        expect(toNumber(Number.MIN_VALUE)).to.eql(Number.MIN_VALUE);
+    });
+    it("returning NaN when value is an array", () =>{
+        if(expect(isObject([1, 2, 3])).to.be.true){
+            expect(toNumber([1, 2, 3])).to.be.NaN;
+        }
+    });
+    it("returning NaN when value is a Symbol", () =>{
+        if(expect(isSymbol(mySymbol)).to.be.true){
+            expect(toNumber(mySymbol)).to.be.NaN;
+        }
+    });
+})
