2839e0d539532632b49ca08392e96bed0b03fa97
Khang Le Thanh <khang.lethanh@lionbridge.com>
2021-11-05 09:26:49 +0100

    unit teste for isSymbol

--- src/tests/isSymbol.test.js
+++ src/tests/isSymbol.test.js
@@ -1,3 +1,25 @@
 import chai from "chai"
 import isSymbol from "../isSymbol.js"
 const expect = chai.expect
+const mySymbol = Symbol('hello');
+const text = "not running"
+const myObject ={a:1, b:2}
+const myArray = [{a:1}, {b:2}];
+
+describe("isSymbol", () => {
+    it("return true when value is a symbol", () =>{
+        expect(isSymbol(mySymbol)).to.be.true;
+    });  
+    it("return false when value is a string", () =>{
+        expect(isSymbol(text)).to.be.false;
+    }); 
+    it("return false when value is an object", () =>{
+        expect(isSymbol(myObject)).to.be.false;
+    });  
+    it("return false when value is an array", () =>{
+        expect(isSymbol(myArray)).to.be.false;
+    });
+    it("return false when value is null", () =>{
+        expect(isSymbol(null)).to.be.false;
+    }); 
+})
