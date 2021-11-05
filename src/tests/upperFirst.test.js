cba7b0ed1365ac0e7ddeae1d9141a98a1955e092
Khang Le Thanh <khang.lethanh@lionbridge.com>
2021-11-05 09:55:38 +0100

    unit test for upperFirst

--- src/tests/upperFirst.test.js
+++ src/tests/upperFirst.test.js
@@ -1,3 +1,31 @@
 import chai from "chai"
 import upperFirst from "../upperFirst.js"
 const expect = chai.expect
+const lowerCaseString = "hello world"
+const firstLowerCaseString = "hElLo wOrLd"
+const upperCaseString = "Hello world"
+const capitalizedString = "COFFE"
+const firstCapitalizedString = "COFFE house"
+const emptyString = ""
+
+
+describe("upperFirst", () => {
+    it("returning a first character lowercase of a string to be uppercase", () =>{
+        expect(upperFirst(lowerCaseString)).to.equal("Hello world")
+    });  
+    it("returning a first character lowercase of a mixed lowercase and uppercase string to be uppercase", () =>{
+        expect(upperFirst(firstLowerCaseString)).to.equal("HElLo wOrLd")
+    }); 
+    it("returning a same capitalized string", () =>{
+        expect(upperFirst(capitalizedString)).to.equal("COFFE")
+    });
+    it("returning a same first capitalized string", () =>{
+        expect(upperFirst(firstCapitalizedString)).to.equal("COFFE house")
+    });
+    it("returning a same uppercase string", () =>{
+        expect(upperFirst(upperCaseString)).to.equal("Hello world")
+    });
+    it("returning a same empty string", () =>{
+        expect(upperFirst(emptyString)).to.equal("")
+    });
+})
