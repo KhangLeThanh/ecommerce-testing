e3012e0f4ee133824a08b500ed04802481b3072a
Petri Kosonen <petri.kosonen@tuni.fi>
2021-11-16 21:22:48 +0200

    tests for isBuffer

--- src/tests/isBuffer.test.js
+++ src/tests/isBuffer.test.js
@@ -1,3 +1,37 @@
 import chai from "chai"
 import isBuffer from "../isBuffer.js"
 const expect = chai.expect
+
+const symbol = Symbol('hello');
+const text = "not running"
+const object ={a:1, b:2}
+const array = [{a:1}, {b:2}];
+const buffer = Buffer.from(array);
+
+describe("isBuffer", () => {
+        
+    it("return true when value is a buffer", () =>{
+        expect(isBuffer(buffer)).to.be.true;
+    });  
+
+    it("return false when value is a symbol", () =>{
+        expect(isBuffer(symbol)).to.be.false;
+    });  
+
+    it("return false when value is a string", () =>{
+        expect(isBuffer(text)).to.be.false;
+    }); 
+
+    it("return false when value is an object", () =>{
+        expect(isBuffer(object)).to.be.false;
+    });  
+
+    it("return false when value is an array", () =>{
+        expect(isBuffer(array)).to.be.false;
+    });
+
+    it("return false when value is null", () =>{
+        expect(isBuffer(null)).to.be.false;
+    }); 
+
+})
