28026eb5a1eb6389e840341d615b23cb3d6d08f4
Khang Le Thanh <khang.lethanh@lionbridge.com>
2021-11-25 16:05:44 +0200

    fix isbuffer

--- src/tests/isBuffer.test.js
+++ src/tests/isBuffer.test.js
@@ -7,27 +7,29 @@ const text = "not running"
 const object ={a:1, b:2}
 const array = [{a:1}, {b:2}];
 const buffer = Buffer.from(array);
+var uint8 = new Uint8Array(2);
 
 describe("isBuffer", () => {
-        
     it("return true when value is a buffer", () =>{
-        expect(isBuffer(buffer)).to.be.false;
+        expect(Buffer.isBuffer(buffer)).to.be.true;
     });  
-
+    it("return false when value is not a buffer", () =>{
+        expect(Buffer.isBuffer(uint8)).to.be.false;
+    });  
     it("return false when value is a symbol", () =>{
-        expect(isBuffer(symbol)).to.be.false;
+        expect(Buffer.isBuffer(symbol)).to.be.false;
     });  
 
     it("return false when value is a string", () =>{
-        expect(isBuffer(text)).to.be.false;
+        expect(Buffer.isBuffer(text)).to.be.false;
     }); 
 
     it("return false when value is an object", () =>{
-        expect(isBuffer(object)).to.be.false;
+        expect(Buffer.isBuffer(object)).to.be.false;
     });  
 
     it("return false when value is an array", () =>{
-        expect(isBuffer(array)).to.be.false;
+        expect(Buffer.isBuffer(array)).to.be.false;
     });
 
     it("return false when value is null", () =>{
